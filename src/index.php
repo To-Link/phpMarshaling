b42b6778ac926f0a4b079f33e20b43489517f379
Cogi <dbwodlf3@naver.com>
2022-03-16 02:21:43 +0900

    update

--- src/index.php
+++ src/index.php
@@ -3,262 +3,271 @@
  * @property SQlite3 $DB
 */
 class phpMarshaling{ 
-	public static $dbPath = "/usr/share/phpMarshaling";
-	public static $max_db = 20;
-
-	public function __construct()
-	{
-	}
-	
-	public function connect($db_number = NULL){
-		if(!isset($db_number)) { 
-			$db_number = rand(0, self::$max_db-1);
-			$this->DB = new SQLite3(self::$dbPath."phpMarshaling.sqlite".$db_number);
-			$this->DB->busyTimeout(self::$max_db*0.5+30);
-			return;
-		}
-
-		$this->DB = new SQLite3(self::$dbPath."phpMarshaling.sqlite".$db_number);
-		$this->DB->busyTimeout(self::$max_db*10);
-		return;
-	}
-
-	public static function init($username=NULL) {
-		$file = fopen(self::$dbPath.".phpMarshalingInit", "w");
-		fwrite($file, " ");
-		fclose($file);
-
-		for($i=0; $i<self::$max_db; $i++){
-			chmod(self::$dbPath."phpMarshaling.sqlite".$i, 0666);
-
-			if(isset($username)) {
-				chown($username, $username);
-				chmod(self::$dbPath."phpMarshaling.sqlite".$i, 0666);
-			}
-		}
-	}
-
-	public function clearInit() {
-		unlink(self::$dbPath.".phpMarshalingInit");
-	}
-
-	public static function isInit() {
-		return file_exists(self::$dbPath.".phpMarshalingInit");
-	}
-
-	public function createTable($tableName) {
-		$escaped_tableName = Sqlite3::escapeString(($tableName));
-
-		return $this->DB->exec("
-			CREATE TABLE $escaped_tableName(
-				dataId TEXT,
-				value TEXT
-			)
-		");
-	}
-
-	public function createTableMultipleColumn($tableName, $columns){
-		$escaped_tableName = Sqlite3::escapeString($tableName);
-
-		$query = "			CREATE TABLE $escaped_tableName(
-			dataId TEXT";
-
-		foreach($columns as $column) {
-			$escaped_columnName = Sqlite3::escapeString($column);
-			$query .= ", ".$escaped_columnName." TEXT";
-		}
-
-		$query .=");";
-
-		return $this->DB->exec($query);
-	}
-
-	public function insert($tableName, $dataId, $value=0) {
-		$escaped_tableName = Sqlite3::escapeString($tableName);
-		$escaped_dataId = Sqlite3::escapeString($dataId);
-		$escaped_value = Sqlite3::escapeString($value);
-
-		$insert_query = "INSERT INTO $escaped_tableName(dataId, value) 
-			VALUES('$escaped_dataId', '$escaped_value')
-		";
+    public static $dbPath = "/usr/share/phpMarshaling";
+    public static $max_db = 20;
+
+    public function __construct()
+    {
+    }
+    
+    public function connect($db_number = NULL){
+        if(!isset($db_number)) { 
+            $db_number = rand(0, self::$max_db-1);
+            $this->DB = new SQLite3(self::$dbPath."phpMarshaling.sqlite".$db_number);
+            $this->DB->busyTimeout(self::$max_db*0.5+30);
+            return;
+        }
+
+        $this->DB = new SQLite3(self::$dbPath."phpMarshaling.sqlite".$db_number);
+        $this->DB->busyTimeout(self::$max_db*10);
+        return;
+    }
+
+    public static function init($username=NULL) {
+        $file = fopen(self::$dbPath.".phpMarshalingInit", "w");
+        fwrite($file, " ");
+        fclose($file);
+
+        for($i=0; $i<self::$max_db; $i++){
+            chmod(self::$dbPath."phpMarshaling.sqlite".$i, 0666);
+
+            if(isset($username)) {
+                chown($username, $username);
+                chmod(self::$dbPath."phpMarshaling.sqlite".$i, 0666);
+            }
+        }
+    }
+
+    public function clearInit() {
+        unlink(self::$dbPath.".phpMarshalingInit");
+    }
+
+    public static function isInit() {
+        return file_exists(self::$dbPath.".phpMarshalingInit");
+    }
+
+    public function createTable($tableName) {
+        $escaped_tableName = Sqlite3::escapeString(($tableName));
+
+        return $this->DB->exec("
+            CREATE TABLE $escaped_tableName(
+                dataId TEXT,
+                value TEXT
+            )
+        ");
+    }
+
+    public function createTableMultipleColumn($tableName, $columns){
+        $escaped_tableName = Sqlite3::escapeString($tableName);
+
+        $query = "			CREATE TABLE $escaped_tableName(
+            dataId TEXT";
+
+        foreach($columns as $column) {
+            $escaped_columnName = Sqlite3::escapeString($column);
+            $query .= ", ".$escaped_columnName." TEXT";
+        }
+
+        $query .=");";
+
+        return $this->DB->exec($query);
+    }
+
+    public function insert($tableName, $dataId, $value=0) {
+        $escaped_tableName = Sqlite3::escapeString($tableName);
+        $escaped_dataId = Sqlite3::escapeString($dataId);
+        $escaped_value = Sqlite3::escapeString($value);
 
-		return $this->DB->exec($insert_query);
-	}
+        $insert_query = "INSERT INTO $escaped_tableName(dataId, value) 
+            VALUES('$escaped_dataId', '$escaped_value')
+        ";
 
-	public function insertColumns($tableName, $dataSet) {
-		$escaped_tableName = Sqlite3::escapeString($tableName);
-		$columns = array();
-		$values = array();
+        return $this->DB->exec($insert_query);
+    }
 
-		foreach($dataSet as $data) {
-			$escaped_columnName = Sqlite3::escapeString($data[0]);
-			$escaped_columnValue = Sqlite3::escapeString($data[1]);
-			array_push($columns, $escaped_columnName);
-			array_push($values, $escaped_columnValue);
-		}
+    public function insertColumns($tableName, $dataSet) {
+        $escaped_tableName = Sqlite3::escapeString($tableName);
+        $columns = array();
+        $values = array();
 
-		$insert_query = "INSERT INTO $escaped_tableName($columns[0]";
+        foreach($dataSet as $data) {
+            $escaped_columnName = Sqlite3::escapeString($data[0]);
+            $escaped_columnValue = Sqlite3::escapeString($data[1]);
+            array_push($columns, $escaped_columnName);
+            array_push($values, $escaped_columnValue);
+        }
 
-		for($i=1; $i<count($columns); $i++) {
-			$insert_query .= ", $columns[$i]";
-		}
+        $insert_query = "INSERT INTO $escaped_tableName($columns[0]";
 
-		$insert_query .= ") VALUES('$values[0]'";
+        for($i=1; $i<count($columns); $i++) {
+            $insert_query .= ", $columns[$i]";
+        }
 
-		for($i=1; $i<count($values); $i++) {
-			$insert_query .= ", '$values[$i]'";
-		}
+        $insert_query .= ") VALUES('$values[0]'";
 
-		$insert_query .= ");";
+        for($i=1; $i<count($values); $i++) {
+            $insert_query .= ", '$values[$i]'";
+        }
 
-		return $this->DB->exec($insert_query);
-	}
+        $insert_query .= ");";
 
-	/**
-	 * @param [int](string, string, string) $dataSet 
-	 * 		array  of tuple ($tableName, $dataId, $value)
-	*/
-	public function insertTransaction($dataSet) {
-		$query = 'BEGIN;';
+        return $this->DB->exec($insert_query);
+    }
 
-		foreach($dataSet as $data) {
+    /**
+     * @param [int](string, string, string) $dataSet 
+     * 		array  of tuple ($tableName, $dataId, $value)
+    */
+    public function insertTransaction($dataSet) {
+        $query = 'BEGIN;';
 
-			$escaped_tableName = Sqlite3::escapeString($data[0]);
-			$escaped_dataId = Sqlite3::escapeString($data[1]);
+        foreach($dataSet as $data) {
 
-			if(isset($data[2])) {
-				$escaped_value = Sqlite3::escapeString($data[2]);
-			} 
-			else {
-				$escaped_value = 0;
-			}
+            $escaped_tableName = Sqlite3::escapeString($data[0]);
+            $escaped_dataId = Sqlite3::escapeString($data[1]);
 
-			$query .= "INSERT INTO $escaped_tableName(dataId, value) 
-				VALUES('$escaped_dataId', '$escaped_value');
-			";
-		}
+            if(isset($data[2])) {
+                $escaped_value = Sqlite3::escapeString($data[2]);
+            } 
+            else {
+                $escaped_value = 0;
+            }
 
-		$query .= 'END;';
+            $query .= "INSERT INTO $escaped_tableName(dataId, value) 
+                VALUES('$escaped_dataId', '$escaped_value');
+            ";
+        }
 
-		$this->DB->exec($query);
+        $query .= 'END;';
 
-		return ;
-	}
+        $this->DB->exec($query);
 
-	public function clean($tableName, $dataId) {
-		$escaped_tableName = Sqlite3::escapeString(($tableName));
-		$escaped_dataId = Sqlite3::escapeString(($dataId));
+        return ;
+    }
 
-		$delete_query = "DELETE FROM $escaped_tableName WHERE dataId=$escaped_dataId";
+    public function clean($tableName, $dataId) {
+        $escaped_tableName = Sqlite3::escapeString(($tableName));
+        $escaped_dataId = Sqlite3::escapeString(($dataId));
 
-		return $this->DB->exec($delete_query);
-	}
+        $delete_query = "DELETE FROM $escaped_tableName WHERE dataId=$escaped_dataId";
 
-	public function clean2($tableName, $dataSet) {
-		$escaped_tableName = Sqlite3::escapeString(($tableName));
+        return $this->DB->exec($delete_query);
+    }
 
-		$column = $dataSet[0][0];
-		$value = $dataSet[0][1];
-		$delete_query = "DELETE FROM $escaped_tableName WHERE $column='$value'";
+    public function clean2($tableName, $dataSet) {
+        $escaped_tableName = Sqlite3::escapeString(($tableName));
 
-		for($i=1; $i<count($dataSet); $i++) {
-			$column = $dataSet[$i][0];
-			$value = $dataSet[$i][1];
-			$delete_query .= " AND $column='$value'";
-		}
+        $column = $dataSet[0][0];
+        $value = $dataSet[0][1];
+        $delete_query = "DELETE FROM $escaped_tableName WHERE $column='$value'";
 
-		return $this->DB->exec($delete_query);
+        for($i=1; $i<count($dataSet); $i++) {
+            $column = $dataSet[$i][0];
+            $value = $dataSet[$i][1];
+            $delete_query .= " AND $column='$value'";
+        }
 
-	}
+        return $this->DB->exec($delete_query);
 
-	public function cleanAll($tableName) {
-		$escaped_tableName = Sqlite3::escapeString(($tableName));
-		$delete_query = "DELETE FROM $escaped_tableName";
+    }
 
-		return $this->DB->exec($delete_query);
-	}
+    public function clean3($tableName, $rowId) {
+        $escaped_tableName = Sqlite3::escapeString(($tableName));
+        $escaped_rowId = Sqlite3::escapeString(($rowId));
 
-	public function get($tableName, $dataId) {
-		$escaped_tableName = Sqlite3::escapeString(($tableName));
+        $delete_query = "DELETE FROM $escaped_tableName WHERE rowid=$escaped_rowId";
 
-		$select_query = "SELECT * FROM $escaped_tableName";
+        return $this->DB->exec($delete_query);
+    }
 
-		return $this->DB->exec($select_query);
-	}
-	
-	public function getAll($tableName) {
-		$escaped_tableName = Sqlite3::escapeString(($tableName));
+    public function cleanAll($tableName) {
+        $escaped_tableName = Sqlite3::escapeString(($tableName));
+        $delete_query = "DELETE FROM $escaped_tableName";
 
-		$get_query = "SELECT dataId, value, COUNT(dataId) AS sum FROM $escaped_tableName GROUP BY dataId";
+        return $this->DB->exec($delete_query);
+    }
 
-		$queryResult = $this->DB->query($get_query);
-		$rows = array();
+    public function get($tableName, $dataId) {
+        $escaped_tableName = Sqlite3::escapeString(($tableName));
 
-		while($row = $queryResult->fetchArray()) {
-			array_push($rows, $row);
-		}
+        $select_query = "SELECT * FROM $escaped_tableName";
 
-		return $rows;
-	}
+        return $this->DB->exec($select_query);
+    }
+    
+    public function getAll($tableName) {
+        $escaped_tableName = Sqlite3::escapeString(($tableName));
 
-	public function getAll2($tableName, $columns) {
-		$escaped_tableName = Sqlite3::escapeString($tableName);
+        $get_query = "SELECT dataId, value, COUNT(dataId) AS sum FROM $escaped_tableName GROUP BY dataId";
 
-		$get_query = " 
-			SELECT dataId, COUNT(dataId) AS sum";
+        $queryResult = $this->DB->query($get_query);
+        $rows = array();
 
-		foreach($columns as $column){
-			$get_query .= ", $column";
-		}
+        while($row = $queryResult->fetchArray()) {
+            array_push($rows, $row);
+        }
 
-		$get_query .= " FROM $escaped_tableName GROUP BY dataId";
-		
-		foreach($columns as $column) {
-			$escaped_columnName = Sqlite3::escapeString($column);
-			$get_query .=", $escaped_columnName";
-		}
+        return $rows;
+    }
 
-		$queryResult = $this->DB->query($get_query);
-		$rows = array();
+    public function getAll2($tableName, $columns) {
+        $escaped_tableName = Sqlite3::escapeString($tableName);
 
-		while($row = $queryResult->fetchArray()) {
-			array_push($rows, $row);
-		}
+        $get_query = " 
+            SELECT dataId, COUNT(dataId) AS sum";
 
-		return $rows;
-	}
+        foreach($columns as $column){
+            $get_query .= ", $column";
+        }
 
-	public function getAllWithClean($tableName) {
-		$escaped_tableName = Sqlite3::escapeString(($tableName));
+        $get_query .= " FROM $escaped_tableName GROUP BY dataId";
+        
+        foreach($columns as $column) {
+            $escaped_columnName = Sqlite3::escapeString($column);
+            $get_query .=", $escaped_columnName";
+        }
 
-		self::$DB->exec("BEGIN;");
-		$query_result = self::$DB->query("
-		BEGIN;
-			SELECT dataId, value, COUNT(dataId) AS sum FROM $escaped_tableName GROUP BY dataId;
-			DELETE FROM $escaped_tableName;
-		END;
-		");
+        $queryResult = $this->DB->query($get_query);
+        $rows = array();
 
-		$rows = array();
+        while($row = $queryResult->fetchArray()) {
+            array_push($rows, $row);
+        }
 
-		while($row = $query_result->fetchArray()) {
-			array_push($rows, $row);
-		}
+        return $rows;
+    }
 
-		return $rows;
-	}
+    public function getAllWithClean($tableName) {
+        $escaped_tableName = Sqlite3::escapeString(($tableName));
 
-	public function beginTransaction(){
-		$this->DB->exec("BEGIN EXCLUSIVE;");
-	}
+        self::$DB->exec("BEGIN;");
+        $query_result = self::$DB->query("
+        BEGIN;
+            SELECT dataId, value, COUNT(dataId) AS sum FROM $escaped_tableName GROUP BY dataId;
+            DELETE FROM $escaped_tableName;
+        END;
+        ");
 
-	public function endTransaction(){
-		$this->DB->exec("END;");
-	}
+        $rows = array();
 
-	public function commit(){
-		$this->DB->exec("COMMIT;");
-	}
+        while($row = $query_result->fetchArray()) {
+            array_push($rows, $row);
+        }
+
+        return $rows;
+    }
+
+    public function beginTransaction(){
+        $this->DB->exec("BEGIN EXCLUSIVE;");
+    }
+
+    public function endTransaction(){
+        $this->DB->exec("END;");
+    }
+
+    public function commit(){
+        $this->DB->exec("COMMIT;");
+    }
 
 }
 ?>
